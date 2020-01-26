4beb16150ef3e2096a16db72c8f9425dbe3cf55c
Skip Hansen <skip@gfrn.org>
2020-01-26 14:55:42 -0800

    Moved codec pin connections into core.

--- cores/g2/opl3/src_v/audio.v
+++ cores/g2/opl3/src_v/audio.v
@@ -5,29 +5,29 @@ module audio(
     input wire  reset25,
 
     input wire codec_bclk_i,
-    output reg  codec_dacdat,
-    output reg  codec_daclrc,
-    input  wire codec_adcdat,
-    output reg  codec_adclrc,
     input  wire [15:0] audio_right_sample,
     input  wire [15:0] audio_left_sample,
     input  wire audio_sample_clk
     );
 
+reg  dacdat;
+reg  daclrc;
+reg  adclrc;
+
 `ifdef CHIP_SCOPE
 (* mark_debug = "TRUE" *) wire  debug_clk25;
 (* mark_debug = "TRUE" *) wire  debug_reset25;
 (* mark_debug = "TRUE" *) wire debug_codec_bclk_i;
-(* mark_debug = "TRUE" *) wire debug_codec_dacdat;
-(* mark_debug = "TRUE" *) wire debug_codec_daclrc;
+(* mark_debug = "TRUE" *) wire debug_dacdat;
+(* mark_debug = "TRUE" *) wire debug_daclrc;
 (* mark_debug = "TRUE" *) wire [15:0] debug_audio_right_sample;
 (* mark_debug = "TRUE" *) wire debug_audio_sample_clk;
 
 assign debug_clk25 = clk25;
 assign debug_reset25 = reset25;
 assign debug_codec_bclk_i = codec_bclk_i;
-assign debug_codec_dacdat = codec_dacdat;
-assign debug_codec_daclrc = codec_daclrc;
+assign debug_dacdat = dacdat;
+assign debug_daclrc = daclrc;
 assign debug_audio_right_sample = audio_right_sample;
 assign debug_audio_sample_clk = audio_sample_clk;
 `endif
@@ -64,31 +64,85 @@ assign debug_audio_sample_clk = audio_sample_clk;
         if(codec_bclk_i && !last_bclk) begin
         end
         else if(!codec_bclk_i && last_bclk) begin
-            codec_daclrc    <= 0;
+            daclrc    <= 0;
             if(start_cycle) begin
                 start_cycle <= 0;
                 bit_cntr     <= 1;
-                codec_daclrc <= 1;
-                codec_dacdat <= audio_left_sample[15];
+                daclrc <= 1;
+                dacdat <= audio_left_sample[15];
             end
             else if (bit_cntr < 16) begin
                 bit_cntr <= bit_cntr + 1;
-                codec_dacdat <= audio_left_sample[~bit_cntr[3:0]];
+                dacdat <= audio_left_sample[~bit_cntr[3:0]];
             end
             else if (bit_cntr < 32) begin
                 bit_cntr <= bit_cntr + 1;
-                codec_dacdat <= audio_right_sample[~bit_cntr[3:0]];
+                dacdat <= audio_right_sample[~bit_cntr[3:0]];
             end
         end
 
         if (reset25) begin
             bit_cntr        <= 6'd32;
-            codec_adclrc    <= 0;
+            adclrc    <= 0;
             last_audio_sample_clk <= 0;
             start_cycle <= 0;
             last_bclk <= 0;
         end
     end
+
+    (* s = "true" *) wire codec_dacdat;
+    (* keep = "true" *) (* s = "true" *) OBUF dacdat_obuf (
+        .I(dacdat),
+        .O(codec_dacdat)
+    );
+
+    (* s = "true" *) wire codec_daclrc;
+    (* keep = "true" *) (* s = "true" *) OBUF daclrc_obuf (
+        .I(daclrc),
+        .O(codec_daclrc)
+    );
+
+    (* s = "true" *) wire codec_adclrc;
+    (* keep = "true" *) (* s = "true" *) OBUF adclrc_obuf (
+        .I(adclrc),
+        .O(codec_adclrc)
+    );
+
+    wire bclk_ddr2;
+    (* keep = "true" *) (* s = "true" *) ODDR2 bclk_oddr2 (
+        .S(1'b0),
+        .R(1'b0),
+        .D0(1'b1),
+        .D1(1'b0),
+        .C0(codec_bclk_i),
+        .C1(!codec_bclk_i),
+        .CE(1'b1),
+        .Q(bclk_ddr2)
+    );
+
+    (* s = "true" *) wire codec_bclk;
+    (* keep = "true" *) (* s = "true" *) OBUF bclk_obuf(
+        .I(bclk_ddr2),
+        .O(codec_bclk)
+    );
+
+    wire mclk_ddr2;
+    (* keep = "true" *) (* s = "true" *) ODDR2 mclk_oddr2 (
+        .S(1'b0),
+        .R(1'b0),
+        .D0(1'b1),
+        .D1(1'b0),
+        .C0(codec_bclk_i),
+        .C1(!codec_bclk_i),
+        .CE(1'b1),
+        .Q(mclk_ddr2)
+    );
+
+    (* s = "true" *) wire codec_mclk;
+    (* keep = "true" *) (* s = "true" *) OBUF mclk_obuf(
+        .I(mclk_ddr2),
+        .O(codec_mclk)
+    );
 
 endmodule
 
