568d8d391a0d2b68314f8bab6b7a7f837758d860
Skip Hansen <skip@gfrn.org>
2020-04-12 10:20:55 -0700

    Build lwip in project's build directory.

--- fw/lwip/include.mk
+++ fw/lwip/include.mk
@@ -1,9 +1,9 @@
 LWIP_LIB_DIR := $(PANO_FW_DIR)/lwip
-LWIP_SRC     := $(LWIP_LIB_DIR)/lwip
-LWIP_LIB     := $(LWIP_LIB_DIR)/build/lwip.a
+LWIP_SRC_DIR := $(abspath $(LWIP_LIB_DIR)/lwip)
+LWIP_LIB     := $(PROJECT_DIR)/build/lwip/lwip.a
 
 EXTRA_LIBS += $(LWIP_LIB)
-INCLUDE_PATH += $(LWIP_SRC)/src/include
+INCLUDE_PATH += $(LWIP_SRC_DIR)/src/include
 
 .PHONY: all
 all:
@@ -12,5 +12,5 @@ $(LWIP_SRC):
 	make -C $(LWIP_LIB_DIR) init
 
 $(LWIP_LIB): | $(LWIP_SRC)
-	make -C $(LWIP_LIB_DIR) PROJECT_INC=$(PROJECT_INC)
+	make -C $(LWIP_LIB_DIR) PROJECT_DIR=$(PROJECT_DIR)
 
